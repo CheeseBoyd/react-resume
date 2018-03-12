17abb5376de019889135643d75c99f4e32be5090
Aaron James Lanot <aaronlanot@gmail.com>
2018-03-12 13:43:32 +0800

    modified font

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -33,7 +33,13 @@ const Styledcard = styled.div`
     border: 1px solid green;
 `;
 
-
+const summaryObj = {
+    "Lorem Ipsum": ["Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum explicabo harum quam ipsa alias molestiae cumque tenetur placeat obcaecati veniam impedit ab, sit, enim sed suscipit! Ratione quod rem enim, repudiandae sit sed quae mollitia iure quidem eum aspernatur, dignissimos minus sunt? Ut reiciendis nostrum atque rerum temporibus perspiciatis est voluptatum architecto?","Sed at condimentum nisl. Aenean lob","i, quis tempor ante pellentesque ac. Cras vitae tempus nisl, non iaculis turpis. Cras non rutrum elit, sed malesuada lacus. Aenean ullamcorper, ante et feugiat molestie, massa arcu tincidunt lectus, et fermentum orci elit quis libero. Mauris risus neque, dignissim sit amet nibh ac, pellentesque interdum metus. Vestibulum non dui urna.","ras id tortor tempus, dignissim dui et, fringilla leo. Nunc at lorem tempus, ullamcorper nibh et, suscipit sem. Quisque nec leo enim. "],
+    "Donec eget": ["amet consectetur adipisicing elit. Ipsum explicabo harum quams neque, sed eleifend ", "Quisque tincidunt luctus mollis. Donec pellentesque vel neque molestie cursus. Mauris lobortis turpis quam, sed finibus"],
+    "Donec of lectus ": ["The Vestibulum vulputate, ante eu suscipit scelerisque, nunc urna tincidunt leo, ut vehicula turpis urna auctor neque. Morbi iaculis lorem ut libero cursus facilisis.", "Tonec pellentesque vel neque molestie cursus"],
+    "consectetur of colors": ["pretium nisi id, mollis eros. Cras id tortor tempus, dignissim dui et, fringilla leo. Nunc at lorem tempus, ullamcorper nibh et, suscipit sem. Quisque nec leo enim. Duis ultrices, justo a congue ultrices, massa justo suscipit purus, sit amet ultrices ipsum turpis sit amet augue. ", "amet", "ultrices", "suscipit"],
+    "ante of scelerisque colors": ["dolor sit, amet consectetur adipisicing elit", "Vestibulum vulputate, ante eu suscipit scelerisque, nunc urna tincidunt leo, ut vehicula turpis urna auctor neque. Morbi iaculis lorem ut libero cursus facilisis. Donec lectus augue, elementum dictum leo vitae, blandit pretium ante. Donec sollicitudin, metus vel maximus commodo, erat nibh venenatis augue, id sodales augue erat vel metus. Donec non viverra lorem, vel tempor dolor. In malesuada, orci sed elementum faucibus, dolor erat eleifend tellus, non consequat massa nulla quis nibh. Aliquam eu risus facilisis, accumsan risus a, vulputate eros. Integer faucibus odio nec euismod aliquam. Morbi aliquet nec mauris eu feugiat. Nullam lorem ante, consectetur in est quis, blandit dapibus magna. In nec tincidunt ligula. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus et vestibulum diam. Phasellus congue ex quis enim vulputate egestas."],
+}
 
 const Homepage = () => {
     return (
@@ -46,7 +52,7 @@ const Homepage = () => {
                  />
             </Styledcard>
             <Styledsummary>
-                <Summary/>
+                <Summary summary={summaryObj} title={'Lorem Dipsum'} />
             </Styledsummary>
         </StyledMain>
     )
--- src/components/homepage/summary/Summary.js
+++ src/components/homepage/summary/Summary.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 /*
     Shape: PropTypes.shape({
     summary: PropTypes.object,
@@ -33,10 +33,6 @@ const StyledlistContainer = styled.div`
     @media (min-width: 992px) { flex-direction: row; }
     @media (min-width: 1200px) { flex-direction: row; }
 `;
-/*
-    display: grid;
-    grid-row-gap: 20px;
- */
 
 const Styledlist = styled.ul`
     flex-basis: 50%;
@@ -49,28 +45,10 @@ const Styledlist = styled.ul`
     @media (min-width: 992px) { flex-direction: column; }
     @media (min-width: 1200px) { flex-direction: column; }
 `;
-/*
-     border: 1px solid grey;
-    padding: 0px;
-    margin: 0px;
-    display: grid;
-    grid-column: 1 / span 3;
-    @media (min-width: 576px) { grid-column: 1 / span 3; }
-    @media (min-width: 768px) { }
-    @media (min-width: 992px) { grid-column: auto; }
-    @media (min-width: 1200px) { grid-column: auto; }
- */
-
 const StyledlistItem = styled.li`
     margin-bottom: 1rem;
 `;
-/*
-    align-self: stretch;
-    line-height: 150%;
-    border: 1px solid grey;
-    list-style: none;
-    padding-left: 1em;
-*/
+
 
 const Styledtitle = styled.div`
     padding-top: 1.5rem;
@@ -82,13 +60,6 @@ const Styledtitle = styled.div`
 
 
 
-/*
-    @media (min-width: 576px) { }
-    @media (min-width: 768px) { }
-    @media (min-width: 992px) { }
-    @media (min-width: 1200px) { }
-
-*/
 
 const Items = (props)=>{
 
@@ -131,21 +102,23 @@ const Summary = (props) =>{
     })
 )
 
+
 // Test props ----------------- >
-const obj = {
-    "Lorem Ipsum": ["Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum explicabo harum quam ipsa alias molestiae cumque tenetur placeat obcaecati veniam impedit ab, sit, enim sed suscipit! Ratione quod rem enim, repudiandae sit sed quae mollitia iure quidem eum aspernatur, dignissimos minus sunt? Ut reiciendis nostrum atque rerum temporibus perspiciatis est voluptatum architecto?","Sed at condimentum nisl. Aenean lob","i, quis tempor ante pellentesque ac. Cras vitae tempus nisl, non iaculis turpis. Cras non rutrum elit, sed malesuada lacus. Aenean ullamcorper, ante et feugiat molestie, massa arcu tincidunt lectus, et fermentum orci elit quis libero. Mauris risus neque, dignissim sit amet nibh ac, pellentesque interdum metus. Vestibulum non dui urna.","ras id tortor tempus, dignissim dui et, fringilla leo. Nunc at lorem tempus, ullamcorper nibh et, suscipit sem. Quisque nec leo enim. "],
-    "Donec eget": ["amet consectetur adipisicing elit. Ipsum explicabo harum quams neque, sed eleifend ", "Quisque tincidunt luctus mollis. Donec pellentesque vel neque molestie cursus. Mauris lobortis turpis quam, sed finibus"],
-    "Donec of lectus ": ["The Vestibulum vulputate, ante eu suscipit scelerisque, nunc urna tincidunt leo, ut vehicula turpis urna auctor neque. Morbi iaculis lorem ut libero cursus facilisis.", "Tonec pellentesque vel neque molestie cursus"],
-    "consectetur of colors": ["pretium nisi id, mollis eros. Cras id tortor tempus, dignissim dui et, fringilla leo. Nunc at lorem tempus, ullamcorper nibh et, suscipit sem. Quisque nec leo enim. Duis ultrices, justo a congue ultrices, massa justo suscipit purus, sit amet ultrices ipsum turpis sit amet augue. ", "amet", "ultrices", "suscipit"],
-    "ante of scelerisque colors": ["dolor sit, amet consectetur adipisicing elit", "Vestibulum vulputate, ante eu suscipit scelerisque, nunc urna tincidunt leo, ut vehicula turpis urna auctor neque. Morbi iaculis lorem ut libero cursus facilisis. Donec lectus augue, elementum dictum leo vitae, blandit pretium ante. Donec sollicitudin, metus vel maximus commodo, erat nibh venenatis augue, id sodales augue erat vel metus. Donec non viverra lorem, vel tempor dolor. In malesuada, orci sed elementum faucibus, dolor erat eleifend tellus, non consequat massa nulla quis nibh. Aliquam eu risus facilisis, accumsan risus a, vulputate eros. Integer faucibus odio nec euismod aliquam. Morbi aliquet nec mauris eu feugiat. Nullam lorem ante, consectetur in est quis, blandit dapibus magna. In nec tincidunt ligula. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus et vestibulum diam. Phasellus congue ex quis enim vulputate egestas."],
-}
-const entries = Object.entries(obj);
+// const obj = {
+//     "Lorem Ipsum": ["Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum explicabo harum quam ipsa alias molestiae cumque tenetur placeat obcaecati veniam impedit ab, sit, enim sed suscipit! Ratione quod rem enim, repudiandae sit sed quae mollitia iure quidem eum aspernatur, dignissimos minus sunt? Ut reiciendis nostrum atque rerum temporibus perspiciatis est voluptatum architecto?","Sed at condimentum nisl. Aenean lob","i, quis tempor ante pellentesque ac. Cras vitae tempus nisl, non iaculis turpis. Cras non rutrum elit, sed malesuada lacus. Aenean ullamcorper, ante et feugiat molestie, massa arcu tincidunt lectus, et fermentum orci elit quis libero. Mauris risus neque, dignissim sit amet nibh ac, pellentesque interdum metus. Vestibulum non dui urna.","ras id tortor tempus, dignissim dui et, fringilla leo. Nunc at lorem tempus, ullamcorper nibh et, suscipit sem. Quisque nec leo enim. "],
+//     "Donec eget": ["amet consectetur adipisicing elit. Ipsum explicabo harum quams neque, sed eleifend ", "Quisque tincidunt luctus mollis. Donec pellentesque vel neque molestie cursus. Mauris lobortis turpis quam, sed finibus"],
+//     "Donec of lectus ": ["The Vestibulum vulputate, ante eu suscipit scelerisque, nunc urna tincidunt leo, ut vehicula turpis urna auctor neque. Morbi iaculis lorem ut libero cursus facilisis.", "Tonec pellentesque vel neque molestie cursus"],
+//     "consectetur of colors": ["pretium nisi id, mollis eros. Cras id tortor tempus, dignissim dui et, fringilla leo. Nunc at lorem tempus, ullamcorper nibh et, suscipit sem. Quisque nec leo enim. Duis ultrices, justo a congue ultrices, massa justo suscipit purus, sit amet ultrices ipsum turpis sit amet augue. ", "amet", "ultrices", "suscipit"],
+//     "ante of scelerisque colors": ["dolor sit, amet consectetur adipisicing elit", "Vestibulum vulputate, ante eu suscipit scelerisque, nunc urna tincidunt leo, ut vehicula turpis urna auctor neque. Morbi iaculis lorem ut libero cursus facilisis. Donec lectus augue, elementum dictum leo vitae, blandit pretium ante. Donec sollicitudin, metus vel maximus commodo, erat nibh venenatis augue, id sodales augue erat vel metus. Donec non viverra lorem, vel tempor dolor. In malesuada, orci sed elementum faucibus, dolor erat eleifend tellus, non consequat massa nulla quis nibh. Aliquam eu risus facilisis, accumsan risus a, vulputate eros. Integer faucibus odio nec euismod aliquam. Morbi aliquet nec mauris eu feugiat. Nullam lorem ante, consectetur in est quis, blandit dapibus magna. In nec tincidunt ligula. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus et vestibulum diam. Phasellus congue ex quis enim vulputate egestas."],
+// }
 // Test props < -----------------
+const entries = Object.entries(props.summary);
+
 
     return(
         <Styledmain className="background-blackboard">
             <Styledtitle>
-                <h3>Lorem ipsum dolor sit amet.</h3>
+                <h3>{props.title}</h3>
             </Styledtitle>
             <StyledlistContainer>
                 {
